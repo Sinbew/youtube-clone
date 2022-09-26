0c17f21a20ecc592600cfaa9a58e7313ac9fd735
Aleksandr Raskulov <alexandr1991r@gmail.com>
2022-09-26 11:07:54 +0300

    commit additional

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import { useState, useEffect } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+import Sidebar from "./Sidebar";
 
-function Feed() {
-  return <div>Feed</div>;
-}
+const Feed = () => {
+  return (
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "2px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+        <Sidebar></Sidebar>
+        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff" }}>
+          Copyright 2022 AR
+        </Typography>
+      </Box>
+      <Box></Box>
+    </Stack>
+  );
+};
 
 export default Feed;
